10245c85bf328cbe9482ce807a87d40062996748
dinesh <dinesh.radjou@etu.univ-amu.fr>
2025-06-03 10:49:28 +0200

    debut: preparation exo 4

---
+++ Exercice_4/fichier_to_matrice.py
@@ -0,0 +1,8 @@
+
+
+with open("../Ressource/Exploration_site/youtube.com/youtube.txt") as ytb :
+    #print(ytb.read())
+    matrice_ytb =[]
+    ytb.read()
+    for line in ytb :
+        print(line)
